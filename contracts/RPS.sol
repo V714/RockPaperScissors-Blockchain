558846c51dbc858cf6048330d38758b2abaeb6fe
Tomasz Lipczyński <vlipczynski@gmail.com>
2021-12-29 17:00:25 +0100

    Update RPS.sol

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.8.10;
 
 contract RPS{
     // hashed so the move is not so easy visible
-    bytes32 constant ROCK = keccak256(abi.encodePacked("ROCK"));
-    bytes32 constant PAPER = keccak256(abi.encodePacked("PAPER"));
-    bytes32 constant SCISSORS = keccak256(abi.encodePacked("SCISSORS"));
-    uint counter = 0;
+    bytes32 ROCK;
+    bytes32 PAPER;
+    bytes32 SCISSORS;
+    uint counter;
+
+    constructor(){
+        ROCK = keccak256(abi.encodePacked("ROCK"));
+        PAPER = keccak256(abi.encodePacked("PAPER"));
+        SCISSORS = keccak256(abi.encodePacked("SCISSORS"));
+        counter = 0;
+    }
 
     event Game(bool didYouWin);
 
@@ -16,36 +23,27 @@ contract RPS{
         counter++;
         _;
     }
-    // moves are intigers between 0 - 2, but to compare hashed ones we need to convert them
-    function choiceToMove(uint8 _choice) private pure returns (bytes32) {
+    
+    function choiceToMove(uint8 _choice) private view returns (bytes32) {
         if(_choice == 0) { return ROCK; }
         else if(_choice == 1) { return PAPER; }
         return SCISSORS;
     }
 
-    /*
-    // For test purposes only
-    function testChoiceToMove(uint8 _choice) public pure returns (bytes32) { // because original function is private
-        return choiceToMove(_choice);
+    function justTest() public pure returns(uint256) {
+        return 714;
     }
-    function getMoves() public pure returns (bytes32,bytes32,bytes32) { // just for test cases
-        return (ROCK,PAPER,SCISSORS);
-    } 
-    */
-
 
     // play function which returns if you win or not
-    function play(uint8 _choice) public tick {
+    function play(uint8 _choice) public tick returns(bool) {
         bytes32 enemyMove = choiceToMove(uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty,counter)))%3));
         bytes32 playerMove = choiceToMove(_choice % 3);
-        bool win;
-        if(playerMove == ROCK && enemyMove == PAPER){win = false;}
-        else if(playerMove == ROCK && enemyMove == SCISSORS){win = true;}
-        else if(playerMove == PAPER && enemyMove == ROCK){win = true;}
-        else if(playerMove == PAPER && enemyMove == SCISSORS){win = false;}
-        else if(playerMove == SCISSORS && enemyMove == ROCK){win = false;}
-        else if(playerMove == SCISSORS && enemyMove == PAPER){win = true;}
-        else {win = false;} // in case players made the same move 
-        emit Game(win);
+        if(playerMove == ROCK && enemyMove == PAPER){return false;}
+        else if(playerMove == ROCK && enemyMove == SCISSORS){return true;}
+        else if(playerMove == PAPER && enemyMove == ROCK){return true;}
+        else if(playerMove == PAPER && enemyMove == SCISSORS){return false;}
+        else if(playerMove == SCISSORS && enemyMove == ROCK){return false;}
+        else if(playerMove == SCISSORS && enemyMove == PAPER){return true;}
+        else {return false;} // in case players made the same move 
     }
 }
